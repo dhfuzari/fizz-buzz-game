4cb8b415d075da9b2cf54bc76007d65a2f1abe0b
Daniel H. Fuzari <dhfuzari@gmail.com>
2021-03-03 11:46:36 -0300

    test: check text string values tha will be shown

--- src/tests/fizzBuzzMessages.spec.js
+++ src/tests/fizzBuzzMessages.spec.js
@@ -3,8 +3,14 @@ import FizzBuzzMessages from '../fizzBuzzMessages';
 
 describe('FizzBuzzMessage function', () => {
   context('Possible returning values', () => {
+    const fizzBuzzMessages = FizzBuzzMessages();
     it('should return a FizzBuzzMessages object', () => {
-      expect(FizzBuzzMessages()).to.include.all.keys('fizz', 'buzz', 'fizzBuzz');
+      expect(fizzBuzzMessages).to.include.all.keys('fizz', 'buzz', 'fizzBuzz');
+    });
+    it('should have the value "fizz", "buzz" and "FizzBuzz"', () => {
+      expect(fizzBuzzMessages.fizz).to.be.equal('Fizz');
+      expect(fizzBuzzMessages.buzz).to.be.equal('Buzz');
+      expect(fizzBuzzMessages.fizzBuzz).to.be.equal('FizzBuzz');
     });
   });
 });
