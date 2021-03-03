fc91cfbd784174f2ac01b2d989d292faa9cdd008
Daniel H. Fuzari <dhfuzari@gmail.com>
2021-03-03 11:49:49 -0300

    refactor: updated FizzBuzz function to use FizzBuzzMessages values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import FizzBuzzMessage from './fizzBuzzMessages';
+
+const fizzBuzzMessage = FizzBuzzMessage();
+
 const FizzBuzz = (number) => {
   if (number === 0) return 0;
-  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
-  if (number % 3 === 0) return 'Fizz';
-  if (number % 5 === 0) return 'Buzz';
+  if (number % 3 === 0 && number % 5 === 0) return fizzBuzzMessage.fizzBuzz;
+  if (number % 3 === 0) return fizzBuzzMessage.fizz;
+  if (number % 5 === 0) return fizzBuzzMessage.buzz;
   return number;
 };
 
